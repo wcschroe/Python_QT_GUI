43fb083b0ebb1b91f4acf4e77d4dafacc262f393
William Schroeder <william.schroeder78@gmail.com>
2020-10-04 20:26:19 -0700

    simple read from CSV and plot func

--- gui.py
+++ gui.py
@@ -1,36 +1,85 @@
-import sys
-import random
-from PySide2.QtWidgets import (QApplication, QLabel, QPushButton, QVBoxLayout, QWidget)
-from PySide2.QtCore import Slot, Qt
+# USE THIS TO INSTALL STUFF: "pip install PySide2 pyqtgraph"
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QLayout, QVBoxLayout, QHBoxLayout, QGridLayout, QSizePolicy, QLabel, QMainWindow, QFrame, QTabWidget, QComboBox, QLineEdit
+from PySide2.QtCore import QTimer
+import pyqtgraph as pg
+from pyqtgraph import PlotWidget, plot
+from PySide2.QtGui import QFont
+import sys, os
+from random import randint
 
-class MyWidget(QWidget):
-    def __init__(self):
-        QWidget.__init__(self)
+# Create Application
+app = QApplication([])
+appFont = QFont()
+appFont.setPointSize(15)
 
-        self.hello = ["Hallo Welt", "你好，世界", "Hei maailma",
-            "Hola Mundo", "Привет мир"]
+# first page layou
+tab1 = QWidget()
+x = [0,1,2,3,4]  # x points
+y = [0,1,2,3,4]  # y points
 
-        self.button = QPushButton("Click me!")
-        self.text = QLabel("Hello World")
-        self.text.setAlignment(Qt.AlignCenter)
+graph = pg.PlotWidget()
+graph.enableMouse(False)
+graph.setTitle("Data From CSV")
+graph.setBackground((255,255,255))
+graph.addLegend()
+graph.showGrid(x = True, y = True)
+graph.setLabel("bottom", "Column 1")
+graph.setLabel("left", "Column 2")
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
+pen = pg.mkPen(color=(0,0,255)) #blue line
+line = graph.plot(x, y, pen = pen)
+line.setData(x, y)
 
-        # Connecting the signal
-        self.button.clicked.connect(self.magic)
+tab1Layout = QGridLayout()
+tab1Layout.columnCount = 2
+tab1Layout.rowCount = 2
+tab1Layout.setColumnStretch(0, 2)
+tab1Layout.setColumnStretch(1, 1)
 
-    @Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+fileLocation = QLineEdit()
+fileLocation.setPlaceholderText("CSV File Location")
+openFileButton = QPushButton("Open File")
+errorOutput = QLabel("")
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
+fileOpenLayout = QVBoxLayout()
+fileOpenLayout.addWidget(fileLocation)
+fileOpenLayout.addWidget(errorOutput)
+fileOpenLayout.addWidget(openFileButton)
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
 
-    sys.exit(app.exec_())
+tab1Layout.addWidget(graph, 0, 0)
+tab1Layout.addLayout(fileOpenLayout, 0, 1)
+
+tab1.setLayout(tab1Layout)
+
+# Setup Tabs
+tabs = QTabWidget()
+tabs.setTabPosition(QTabWidget.North)
+tab2 = QWidget()
+tab3 = QWidget()
+tabs.addTab(tab1, "Tab 1")
+tabs.addTab(tab2, "Tab 2")
+tabs.addTab(tab3, "Tab 3")
+
+# add data to graph
+def PlotCSV():
+    line.clear()
+    try:
+        csv = open(fileLocation.text(), 'r') #open csv for reading
+        pairs = csv.read().splitlines()
+        for pair in pairs:
+            x.append(float(pair.split(',')[0]))
+            y.append(float(pair.split(',')[1]))
+    except FileNotFoundError:
+        errorOutput.setText(fileLocation.text() + " Not Found (Try sine.csv)")
+    line.setData(x, y)
+
+openFileButton.released.connect(PlotCSV)
+
+
+# Window Setup
+win = QMainWindow()
+win.setCentralWidget(tabs)
+win.resize(800, 480)
+win.show()
+sys.exit (app.exec_())
